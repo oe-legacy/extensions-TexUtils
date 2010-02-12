f5b1f22540b4085620bb2a14173f44768a69605d
ptx <ptx@openengine.dk>
2010-02-12 17:59:26 +0100

    New texture interface
    Ignore-this: 980a35251f42ba1f960009606355a532
    
    darcs-hash:20100212165926-2a962-37593fdce14e45d58004ced76dd01d9e6af6e9c2.gz

--- Resources/EmptyTextureResource.h
+++ Resources/EmptyTextureResource.h
@@ -22,73 +22,48 @@ class EmptyTextureResource;
 
 typedef boost::shared_ptr<EmptyTextureResource> EmptyTextureResourcePtr;
 
-        class EmptyTextureResource : public ITextureResource {
-        private:
-            unsigned int width;
-            unsigned int height;
-            unsigned int depth;
-            unsigned char* data;
-            int id;
-            ColorFormat format;
+class EmptyTextureResource : public ITextureResource {
+private:
 
-            boost::weak_ptr<EmptyTextureResource> weak_this;
+    boost::weak_ptr<EmptyTextureResource> weak_this;
+    
+    EmptyTextureResource(unsigned int w, unsigned int h, unsigned int d) 
+        : ITextureResource() {
 
-            EmptyTextureResource(unsigned int w, unsigned int h, unsigned int d) 
-                : width(w), height(h), depth(d), data(NULL), id(0) {
-                switch (depth){
-                case 8: format = LUMINANCE; break;
-                case 24: format = RGB; break;
-                case 32: format = RGBA; break;                    
-                }
-                data = new unsigned char[width*height*depth/8];
-            }
-        public:
-            static EmptyTextureResourcePtr Create(unsigned int w,
-                                                  unsigned int h,
-                                                  unsigned int d) {
-                EmptyTextureResourcePtr ptr(new EmptyTextureResource(w,h,d));
-                ptr->weak_this = ptr;
-                return ptr;
-            }
+        this->width = w;
+        this->height = h;
+        this->channels = d/8;
+                
+        switch (d){
+        case 8: this->format = LUMINANCE; break;
+        case 24: this->format = RGB; break;
+        case 32: this->format = RGBA; break;                    
+        }
+        this->data = new unsigned char[width*height*d/8];
+    }
+public:
+    static EmptyTextureResourcePtr Create(unsigned int w,
+                                          unsigned int h,
+                                          unsigned int d) {
+        EmptyTextureResourcePtr ptr(new EmptyTextureResource(w,h,d));
+        ptr->weak_this = ptr;
+        return ptr;
+    }
             
-            static EmptyTextureResourcePtr Clone(ITextureResourcePtr tex) {
-                unsigned int w = tex->GetWidth();
-                unsigned int h = tex->GetHeight();
-                unsigned int d = tex->GetDepth();
-                EmptyTextureResourcePtr ptr = Create(w,h,d);
+    ~EmptyTextureResource() { exit(-1);  delete[] data; }
+    void Load() {}
+    void Unload() {} //delete data; }
 
-                unsigned char* from = tex->GetData();
-                unsigned char* to = ptr->GetData();
+    void RebindTexture() {
+        changedEvent.Notify(TextureChangedEventArg(EmptyTextureResourcePtr(weak_this)));
+    }
 
-                std::copy(from, from+w*h*d/8, to);
-
-                // for (unsigned int x=0; x<w; x++)
-                //     for (unsigned int y=0; y<h; y++)
-                //         to[x+y*w] = from[x+y*w];
-
-                return ptr;
-            }
-            ~EmptyTextureResource() { delete data; }
-            void Load() {}
-            void Unload() {} //delete data; }
-            int GetID() { return id; }
-            void SetID(int id) { this->id = id; }
-            unsigned int GetWidth() { return width; }
-            unsigned int GetHeight() { return height; }
-            unsigned int GetDepth() { return depth; }
-            unsigned char* GetData() { return data; }
-            ColorFormat GetColorFormat() { return format; }
-
-            void RebindTexture() {
-                changedEvent.Notify(TextureChangedEventArg(EmptyTextureResourcePtr(weak_this)));
-            }
-
-            unsigned char& operator()(const unsigned int x,
-                                      const unsigned int y,
-                                      const unsigned int component) {
-                return data[y*width*depth/8+x*depth/8+component];
-            }
-        };
+    unsigned char& operator()(const unsigned int x,
+                              const unsigned int y,
+                              const unsigned int component) {
+        return data[y*width*channels+x*channels+component];
+    }
+};
 //    }
 //}
 
